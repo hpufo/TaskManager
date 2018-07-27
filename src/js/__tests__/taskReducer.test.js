46847178514e39e7b2eeb8ab9495195e78a47d59
Christopher Stephenson <hpufo2@gmail.com>
2018-07-27 09:57:49 -0400

    reducter tests

---
+++ src/js/__tests__/taskReducer.test.js
@@ -0,0 +1,34 @@
+import reducer from '../reducers/tasksReducer';
+import {ACTIONS} from '../actions/actions';
+
+const initialState = {data:[]};
+const task = {_id: '2E', completed: false, name: 'name', due: '2018-07-11', description: 'test'};
+describe('tasks reducer', () => {
+  it('shoud return the initial state', () => {
+    expect(reducer(undefined, {})).toEqual({data: []});
+  });
+
+  it('should ADD_TODO', () => {
+    expect(reducer(initialState,{type: ACTIONS.ADD_TASK, payload: task}))
+    .toEqual({data: [task]});
+  });
+
+  it('should RECIEVE_TASKS', () => {
+    let task2 = Object.assign({}, task, {name: 'test2'});
+    expect(reducer(initialState, {type: ACTIONS.RECIEVE_TASKS, payload: [task,task2]}))
+    .toEqual({data: [task,task2]});
+  });
+
+  it('should DELETE_TASK', () => {
+    expect(reducer({data:[task]}, {type: ACTIONS.DELETE_TASK, payload: task._id}))
+    .toEqual({data: []});
+  });
+  
+  it('should TOGGLE_COMPLETE', () => {
+    const uncompletedTask = Object.assign({}, task, {completed: true});
+    expect(reducer({data:[task]}, {type: ACTIONS.TOGGLE_COMPLETE, payload: {_id: task._id, value: true}}))
+    .toEqual({data: [uncompletedTask]});
+    expect(reducer({data:[uncompletedTask]}, {type: ACTIONS.TOGGLE_COMPLETE, payload: {_id: task._id, value: false}}))
+    .toEqual({data: [task]});
+  });
+});
